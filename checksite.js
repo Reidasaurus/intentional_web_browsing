c5e081e7e6bec22d83f4c5ac17326385e10c249a
Reidasaurus <reidhlewis91@gmail.com>
2020-05-16 09:28:48 -0400

    Finished core functionality. Options page could use some css, and may need to look into better matching check.

--- checksite.js
+++ checksite.js
@@ -2,21 +2,34 @@
 
 	let currentURL = window.location.href;
 	let syncStoredItems = browser.storage.sync.get();
+
+	// Set styles to inject to center blocked site message
 	let styles = document.createElement('style');
 	styles.innerHTML = 
 		"html, body {height: 100%; margin: 0; padding: 0; width: 100%;}" + 
 		"body {display: table;}" +
 		".parentDiv {display: table-cell; text-align: center; vertical-align: middle;}"
+
+	// On successful storage retrieval, check if url matches any in user specified arrays
+	// and take according action	
 	syncStoredItems.then((res) => {
-		let hrefString = "<h1>Current url: " + currentURL + "<h1>"
-		blockSite(res.blockMessage);
-		
-		// res.reminderSiteStrings.forEach((val) => {
-		// 	let h1String = "<h1>" + val + "</h1>";
-		// 	document.body.innerHTML += h1String;
-		// })
+		// Blocked site check
+		if (res.blockedSiteStrings.some(matchURL)) {
+			blockSite(res.blockMessage);
+		} 
+		// Reminder site check
+		else if (res.reminderSiteStrings.some(matchURL)) {
+			alert(res.reminderMessage);
+		}
 	});
 
+	// Create regex and check for match against currentURL
+	function matchURL(matchString) {
+		let matchRegEx = new RegExp(matchString);
+		return matchRegEx.test(currentURL);
+	}
+
+	// Clear the document body and add a vertical and horizontally centered block message
 	function 	blockSite(message) {
 		let parentDiv = document.createElement("DIV");
 		parentDiv.classList.add("parentDiv")
@@ -28,11 +41,4 @@
 		document.body.appendChild(styles);
 		document.body.appendChild(parentDiv);
 	}
-
-	function blockSiteIfListed(blockAry, blockMessage) {
-
-	}
-
-	
-
 })();
