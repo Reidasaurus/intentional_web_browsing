180d4d00351c9bf70dec49c87dc4f9dea2f8489e
Reidasaurus <reidhlewis91@gmail.com>
2020-05-16 00:19:02 -0400

    Change homepage url in manifest, and set checksite.js to run on all websites. Began implementing clearing the page then injecting styles and block message. Need to add conditional against block and reminder arrays.

--- checksite.js
+++ checksite.js
@@ -1,6 +1,37 @@
-let syncedStorageItems = browser.storage.sync.get();
-let currentURL = window.location.href;
+(function() {
 
-syncedStorageItems.then((res) => {
-	console.log(res);
-});
+	let currentURL = window.location.href;
+	let syncStoredItems = browser.storage.sync.get();
+	let styles = document.createElement('style');
+	styles.innerHTML = 
+		"html, body {height: 100%; margin: 0; padding: 0; width: 100%;}" + 
+		"body {display: table;}" +
+		".parentDiv {display: table-cell; text-align: center; vertical-align: middle;}"
+	syncStoredItems.then((res) => {
+		let hrefString = "<h1>Current url: " + currentURL + "<h1>"
+		blockSite(res.blockMessage);
+		// res.reminderSiteStrings.forEach((val) => {
+		// 	let h1String = "<h1>" + val + "</h1>";
+		// 	document.body.innerHTML += h1String;
+		// })
+	});
+
+	function 	blockSite(message) {
+		let parentDiv = document.createElement("DIV");
+		parentDiv.classList.add("parentDiv")
+		let messageH1 = document.createElement("H1");
+		let messageText = document.createTextNode(message);
+		messageH1.appendChild(messageText);
+		parentDiv.appendChild(messageH1);
+		document.body.innerHTML = ""
+		document.body.append(styles);
+		document.body.appendChild(parentDiv);
+	}
+
+	function blockSiteIfListed(blockAry, blockMessage) {
+
+	}
+
+	
+
+})();
